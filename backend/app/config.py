a389050920fd8334748315b15ae10381d4476e29
Ansh Gupta <115959720+AnshGupta0110@users.noreply.github.com>
2025-09-24 19:18:34 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DB_USER: str = os.getenv("DB_USER", "user")
+    DB_USER: str = os.getenv("DB_USER", "root")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "password")
-    DB_HOST: str = os.getenv("DB_HOST", "db")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
     DB_PORT: str = os.getenv("DB_PORT", "3306")
     DB_NAME: str = os.getenv("DB_NAME", "task_manager_db")
 
